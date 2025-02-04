4754041bd88ec28713740026a55c4f241993e63c
xshini01 <122343181+xshini01@users.noreply.github.com>
2025-02-05 00:09:57 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -13,11 +13,11 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from compel import Compel, ReturnedEmbeddingsType
 from IPython.display import clear_output
 
-# Token set 
 hf_token = None
 token_set = False
 ratings = None
 civitai_token = None
+model_name= None
     
 def save_token(token):
     global hf_token, token_set, ratings, civitai_token
@@ -35,13 +35,16 @@ def save_token(token):
     else:
         return "Continue without token"
 
+def is_sdxl(model_path):
+    return any(keyword in model_path for keyword in ["sd-xl", "sdxl", "xl", "illustrious"])
+
 # auto remove clip_skip if sdxl model
 def clip_skip_visibility(model_id):
-    model_id_lower = model_id.lower()
-    if "sd-xl" in model_id_lower or "sdxl" in model_id_lower or "xl" in model_id_lower:
+    if is_sdxl(model_id.lower()):
         return gr.update(visible=False)
-    else:
-        return gr.update(visible=True)
+    if model_name is not None and is_sdxl(model_name.lower()):
+        return gr.update(visible=False)
+    return gr.update(visible=True)
     
 def download_file(url):
     save_dir="/content/stable-diffusion/models"
@@ -81,9 +84,8 @@ def download_file(url):
 
 # load Model and Lora
 def load_model(model_id, lora_id, btn_check, pipe, progress=gr.Progress(track_tqdm=True)):
+    global model_name
 
-    model_id_lower = model_id.lower()
-    is_xl = any(keyword in model_id_lower for keyword in ["sd-xl", "sdxl", "xl", "illustrious"])
     del pipe
     torch.cuda.empty_cache()
     gc.collect()
@@ -95,9 +97,8 @@ def load_model(model_id, lora_id, btn_check, pipe, progress=gr.Progress(track_tq
             
             download_path = download_file(model_id)
             model_name = os.path.basename(download_path)
-            check_xl = any(keyword in model_name.lower() for keyword in ["sd-xl", "sdxl", "xl", "illustrious"])
             
-            pipeline_class = StableDiffusionXLPipeline if check_xl else StableDiffusionPipeline
+            pipeline_class = StableDiffusionXLPipeline if is_sdxl(model_name.lower()) else StableDiffusionPipeline
             pipe = pipeline_class.from_single_file(
                 download_path,
                 torch_dtype=torch.float16,
@@ -105,8 +106,8 @@ def load_model(model_id, lora_id, btn_check, pipe, progress=gr.Progress(track_tq
                 token=hf_token or None
             )
 
-        # Handle local SDXL model
-        elif is_xl:
+        # Handle huggingFace SDXL model
+        elif is_sdxl(model_id.lower()):
             gr.Info("Loading SDXL model...")
             progress(0.2, desc="Loading SDXL model")
             
@@ -118,7 +119,7 @@ def load_model(model_id, lora_id, btn_check, pipe, progress=gr.Progress(track_tq
                 safety_checker=None if verify_token() else True
             )
 
-        # Handle standard Stable Diffusion model
+        # Handle huggingFace standard Stable Diffusion model
         else:
             gr.Info("Loading standard model...")
             progress(0.2, desc="Loading standard model")
@@ -132,11 +133,7 @@ def load_model(model_id, lora_id, btn_check, pipe, progress=gr.Progress(track_tq
             )
     except Exception as e :
         gr.Warning(f"Loading Failed: {str(e)}")
-        
-            # Enable memory optimizations
-    pipe.enable_xformers_memory_efficient_attention()
-
-    # Load LoRA weights
+    
     if lora_id:
         try:
             gr.Info("Loading LoRA...")
@@ -150,6 +147,7 @@ def load_model(model_id, lora_id, btn_check, pipe, progress=gr.Progress(track_tq
             gr.Info("Proceeding without LoRA")
 
     # Move to GPU
+    pipe.enable_xformers_memory_efficient_attention()
     pipe = pipe.to("cuda")
     gr.Info(f"Load Model {model_id} and {lora_id} Success")
     progress(1, desc="Model loaded successfully")
@@ -241,7 +239,6 @@ checkThemeMode="""
 def generated_imgs(model_id, prompt, negative_prompt, scheduler_name, type_prediction, width, height, steps, scale, clip_skip, num_images,pipe, progress=gr.Progress(track_tqdm=True)):
     all_images = []
     gr.Progress(track_tqdm=True)
-    model_id_lower = model_id.lower()
 
     scheduler_args = {
         "prediction_type": type_prediction,
@@ -251,7 +248,7 @@ def generated_imgs(model_id, prompt, negative_prompt, scheduler_name, type_predi
     pipe.scheduler = set_scheduler(scheduler_name, pipe.scheduler.config, scheduler_args)
 
     for _ in range(num_images):
-        if "sd-xl" in model_id_lower or "sdxl" in model_id_lower or "xl" in model_id_lower:
+       if is_sdxl(model_id.lower()) or (model_name is not None and is_sdxl(model_name.lower())):
             compel = Compel(
                 tokenizer=[pipe.tokenizer, pipe.tokenizer_2],
                 text_encoder=[pipe.text_encoder, pipe.text_encoder_2],
